6be1960e4250dd4c34b5859a6912e148b924e709
PiMastah <pimastah@gmail.com>
2017-01-31 19:20:01 +0100

    [FEATURE] add blacklisting directions that are invalid to speed up selecting a valid move

--- js/runBot.js
+++ js/runBot.js
@@ -2,15 +2,18 @@ const GameManager = require('./game_manager');
 
 const game = new GameManager(4);
 
+let allowedDirections = [0, 1, 2, 3];
+
 // 0: up, 1: right, 2: down, 3: left
-let getRandomDirection = () =>  {
-    min = Math.ceil(0);
-    max = Math.floor(3);
-    return Math.floor(Math.random() * (max - min)) + min;
+const getRandomDirection = () =>  {
+    const min = 0;
+    const max = allowedDirections.length - 1;
+    let val = Math.floor(Math.random() * (max - min)) + min;
+    return allowedDirections[val];
 };
 
-let serialize = (cells) => {
-    let serializedState = [];
+const serialize = (cells) => {
+    const serializedState = [];
     for (let i = 0; i < cells.length; i++) {
         for (let x = 0; x < cells[i].length; x++) {
             if (cells[i][x] === null) {
@@ -23,16 +26,12 @@ let serialize = (cells) => {
     return serializedState;
 };
 
-let getHighestTile = (cells) => {
-    let highestValue = 0;
-    for (let i = 0; i < cells.length; i++) {
-        for (let x = 0; x < cells[i].length; x++) {
-            if (cells[i][x] && highestValue < cells[i][x].value) {
-                highestValue = cells[i][x].value;
-            }
-        }
-    }
-    return highestValue;
+const reduceRow = (acc, tile) => acc.concat(tile && tile.value || 0);
+
+const reduceCells = (acc, row) => acc.concat(row.reduce(reduceRow, []));
+
+const getHighestTile = (cells) => {
+    return Math.max.apply([], cells.reduce(reduceCells, []));
 };
 
 while(!game.isGameTerminated()) {
@@ -48,4 +47,13 @@ while(!game.isGameTerminated()) {
     console.log(dataPoint);
 
     game.move(direction);
+
+    if (!game.moved) {
+        allowedDirections.splice(allowedDirections.indexOf(direction), 1);
+        console.log("BLACKLISTING DIR", direction);
+    } else {
+        allowedDirections = [0, 1, 2, 3];
+    }
+
+    console.log(allowedDirections);
 }
