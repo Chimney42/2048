355bd2e4b946f245f85771f398ea947a08fc0c79
Chimney42 <draggili@gmail.com>
2017-03-07 10:23:02 +0100

    WIP

--- js/machineLearning/agent.js
+++ js/machineLearning/agent.js
@@ -22,6 +22,12 @@ calculateAvg = (arr) => {
 const absoluteScores = [];
 const averageScores = [];
 let scoreChart;
+let directionChart;
+const direction0History = [];
+const direction1History = [];
+const direction2History = [];
+const direction3History = [];
+
 
 plotGameMetadata = () => {
     absoluteScores.push({
@@ -43,15 +49,49 @@ plotGameMetadata = () => {
     }
 
     scoreChart.render();
-};
 
-let rewardChart;
-const absoluteRewards = [];
+    direction0History.push({
+        x: gameCount,
+        y: Math.round((direction0Count / moveCount) * 100) / 100
+    });
 
-plotMoveMetadata = (reward) => {
+    if (direction0History.length > updateMetadataInterval) {
+        direction0History.shift();
+    }
+
+    direction1History.push({
+        x: gameCount,
+        y: Math.round((direction1Count / moveCount) * 100) / 100
+    });
+
+
+    if (direction1History.length > updateMetadataInterval) {
+        direction1History.shift();
+    }
+
+    direction2History.push({
+        x: gameCount,
+        y: Math.round((direction2Count / moveCount) * 100) / 100
+    });
+
+    if (direction2History.length > updateMetadataInterval) {
+        direction2History.shift();
+    }
+
+    direction3History.push({
+        x: gameCount,
+        y: Math.round((direction3Count / moveCount) * 100) / 100
+    });
+
+    if (direction3History.length > updateMetadataInterval) {
+        direction3History.shift();
+    }
+
+    directionChart.render();
+
     absoluteRewards.push({
         x: totalMoveCount,
-        y: reward
+        y: gameManager.score - calculateAvg(absoluteScores)
     });
 
     if (absoluteRewards.length > updateMetadataInterval * 5) {
@@ -59,6 +99,22 @@ plotMoveMetadata = (reward) => {
     }
 
     rewardChart.render();
+
+};
+
+let rewardChart;
+const absoluteRewards = [];
+
+let direction0Count = 0;
+let direction1Count = 0;
+let direction2Count = 0;
+let direction3Count = 0;
+
+
+
+plotMoveMetadata = (reward) => {
+
+
 };
 
 calculateRating = () => {
@@ -79,16 +135,17 @@ let maxMoveCount = 0;
 const minMoveCount = 0;
 let highScore = 0;
 
+
 normalize = (val, max, min) => (val - min) / (max - min);
 
 calculateReward = (action) => {
     const lastRating = calculateRating();
     simulateKeyPress(action);
     let reward = calculateRating() - lastRating;
-    const moveCountFactor = normalize(moveCount, maxMoveCount, minMoveCount);
-    const scoreFactor = normalize(gameManager.score, gameManager.storageManager.getBestScore(), 0);
-    const factor = (moveCountFactor * scoreFactor);
-    reward = reward * factor;
+    //const moveCountFactor = normalize(moveCount, maxMoveCount, minMoveCount);
+    //const scoreFactor = normalize(gameManager.score, gameManager.storageManager.getBestScore(), 0);
+    //const factor = Math.sqrt(moveCountFactor * scoreFactor);
+    //reward = reward * scoreFactor;
     return reward;
 };
 
@@ -105,7 +162,7 @@ serializeState = () => {
 
 const env = {};
 env.getNumStates = () => {
-    return 17;
+    return 16;
 };
 env.getMaxNumActions = () => {
     return 4;
@@ -115,7 +172,7 @@ let agent;
 const spec = {
     update : 'qlearn', // qlearn | sarsa
     gamma : 0.9, // discount factor, [0, 1)
-    epsilon : 0.5, // initial epsilon for epsilon-greedy policy, [0, 1)
+    epsilon : 0.15, // initial epsilon for epsilon-greedy policy, [0, 1)
     alpha : 0.1, // value function learning rate
     experience_add_every : 5, // number of time steps before we add another experience to replay memory
     experience_size : 50000, // size of experience replay memory
@@ -123,6 +180,7 @@ const spec = {
     //spec.tderror_clamp = 1.0; // for robustness
     num_hidden_units : 256 // number of neurons in hidden layer
 };
+let epsilonDiff = 0.15;
 
 let gameCount = 0;
 let totalMoveCount = 0;
@@ -137,23 +195,35 @@ initiateLearning = () => {
             moveCount++;
             maxMoveCount = moveCount > maxMoveCount ? moveCount : maxMoveCount;
             totalMoveCount++;
-            let state = [moveCount].concat(serializeState());
+
+            let state = serializeState();
             const action = agent.act(state); // s is an integer, action is integer
-            const reward = calculateReward(action);
-            plotMoveMetadata(reward);
-            agent.learn(reward); // the agent improves its Q,policy,model, etc.
+            simulateKeyPress(action);
+            //const reward = calculateReward(action);
+            //plotMoveMetadata(reward);
+
+            switch (action) {
+                case 0: direction0Count++; break;
+                case 1: direction1Count++; break;
+                case 2: direction2Count++; break;
+                case 3: direction3Count++; break;
+            }
+
+            //agent.learn(reward); // the agent improves its Q,policy,model, etc.
         } else {
             gameCount++;
             endMoveCounts.push(moveCount);
             if (endMoveCounts.length > updateMetadataInterval) {
                 endMoveCounts.shift();
             }
-            moveCount = 0;
+            const reward = gameManager.score - calculateAvg(absoluteScores)
+            agent.learn(reward);
             plotGameMetadata();
-            if (gameCount % 100 === 0) {
-                const epsilon = agent.epsilon;
-                agent.epsilon = epsilon / 2;
-            }
+            moveCount = 0;
+            direction0Count = 0;
+            direction1Count = 0;
+            direction2Count = 0;
+            direction3Count = 0;
             gameManager.restart();
         }
 
@@ -197,6 +267,34 @@ $('#startSim').on('click', () => {
     });
     rewardChart.render();
 
+    directionChart = new CanvasJS.Chart("directionChart", {
+        title: {
+            text: "directions taken per game"
+        },
+        data: [{
+            type: "line",
+            showInLegend: true,
+            name: "0",
+            dataPoints: direction0History
+        },{
+            type: "line",
+            showInLegend: true,
+            name: "1",
+            dataPoints: direction1History
+        },{
+            type: "line",
+            showInLegend: true,
+            name: "2",
+            dataPoints: direction2History
+        },{
+            type: "line",
+            showInLegend: true,
+            name: "3",
+            dataPoints: direction3History
+        }],
+
+    });
+    directionChart.render();
 
     if (gameManager.over === false) {
         simulation = initiateLearning();
@@ -230,6 +328,7 @@ $('#loadAgent').on('click', () => {
 });
 
 $('#resetScore').on('click', () => {
-   gameManager.storageManager.setBestScore(0);
+    gameManager.restart();
+    gameManager.storageManager.setBestScore(0);
    gameManager.actuate();
 });
