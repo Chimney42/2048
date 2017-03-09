cb3f63e5a2416886c389fe667488b146e81a0f67
Chimney42 <draggili@gmail.com>
2017-03-09 10:31:40 +0100

    send metrics to influx

--- js/machineLearning/agent.js
+++ js/machineLearning/agent.js
@@ -8,7 +8,6 @@ generateRandomKey = () => {
 };
 
 
-const updateMetadataInterval = 100;
 calculateAvg = (arr) => {
     if (arr.length === 0) {
         return 0;
@@ -18,103 +17,6 @@ calculateAvg = (arr) => {
         return acc + val.y;
     }, 0);
     return sum / arr.length;
-};
-const absoluteScores = [];
-const averageScores = [];
-let scoreChart;
-let directionChart;
-const direction0History = [];
-const direction1History = [];
-const direction2History = [];
-const direction3History = [];
-
-
-plotGameMetadata = () => {
-    absoluteScores.push({
-        x: gameCount,
-        y: gameManager.score
-    });
-
-    if (absoluteScores.length > updateMetadataInterval) {
-        absoluteScores.shift();
-    }
-
-    averageScores.push({
-        x: gameCount,
-        y: calculateAvg(absoluteScores)
-    });
-
-    if (averageScores.length > updateMetadataInterval) {
-        averageScores.shift();
-    }
-
-    scoreChart.render();
-
-    direction0History.push({
-        x: gameCount,
-        y: Math.round((direction0Count / moveCount) * 100) / 100
-    });
-
-    if (direction0History.length > updateMetadataInterval) {
-        direction0History.shift();
-    }
-
-    direction1History.push({
-        x: gameCount,
-        y: Math.round((direction1Count / moveCount) * 100) / 100
-    });
-
-
-    if (direction1History.length > updateMetadataInterval) {
-        direction1History.shift();
-    }
-
-    direction2History.push({
-        x: gameCount,
-        y: Math.round((direction2Count / moveCount) * 100) / 100
-    });
-
-    if (direction2History.length > updateMetadataInterval) {
-        direction2History.shift();
-    }
-
-    direction3History.push({
-        x: gameCount,
-        y: Math.round((direction3Count / moveCount) * 100) / 100
-    });
-
-    if (direction3History.length > updateMetadataInterval) {
-        direction3History.shift();
-    }
-
-    directionChart.render();
-
-    absoluteRewards.push({
-        x: totalMoveCount,
-        y: gameManager.score - calculateAvg(absoluteScores)
-    });
-
-    if (absoluteRewards.length > updateMetadataInterval * 5) {
-        absoluteRewards.shift();
-    }
-
-    rewardChart.render();
-
-};
-
-let rewardChart;
-const absoluteRewards = [];
-
-let direction0Count = 0;
-let direction1Count = 0;
-let direction2Count = 0;
-let direction3Count = 0;
-
-
-
-plotMoveMetadata = (reward) => {
-
-
 };
 
 calculateRating = () => {
@@ -130,23 +32,12 @@ calculateHighestTile = () => {
         return val > acc ? val : acc;
     }, 0)
 };
-const endMoveCounts = [];
-let maxMoveCount = 0;
-const minMoveCount = 0;
-let highScore = 0;
-
-
 normalize = (val, max, min) => (val - min) / (max - min);
 
 calculateReward = (action) => {
     const lastRating = calculateRating();
     simulateKeyPress(action);
-    let reward = calculateRating() - lastRating;
-    //const moveCountFactor = normalize(moveCount, maxMoveCount, minMoveCount);
-    //const scoreFactor = normalize(gameManager.score, gameManager.storageManager.getBestScore(), 0);
-    //const factor = Math.sqrt(moveCountFactor * scoreFactor);
-    //reward = reward * scoreFactor;
-    return reward;
+    return calculateRating() - lastRating;
 };
 
 serializeState = () => {
@@ -180,50 +71,22 @@ const spec = {
     //spec.tderror_clamp = 1.0; // for robustness
     num_hidden_units : 256 // number of neurons in hidden layer
 };
-let epsilonDiff = 0.15;
 
-let gameCount = 0;
-let totalMoveCount = 0;
-let moveCount = 0;
+let gameCount = 1;
 
 initiateLearning = () => {
-
     agent = agent || new RL.DQNAgent(env, spec);
 
     return setInterval(function () { // start the learning loop
         if (gameManager.over === false) {
-            moveCount++;
-            maxMoveCount = moveCount > maxMoveCount ? moveCount : maxMoveCount;
-            totalMoveCount++;
-
             let state = serializeState();
             const action = agent.act(state); // s is an integer, action is integer
             simulateKeyPress(action);
-            //const reward = calculateReward(action);
-            //plotMoveMetadata(reward);
-
-            switch (action) {
-                case 0: direction0Count++; break;
-                case 1: direction1Count++; break;
-                case 2: direction2Count++; break;
-                case 3: direction3Count++; break;
-            }
-
-            //agent.learn(reward); // the agent improves its Q,policy,model, etc.
+            const reward = calculateReward(action);
+            logRewardMeasurement(gameCount, reward);
+            agent.learn(reward); // the agent improves its Q,policy,model, etc.
         } else {
             gameCount++;
-            endMoveCounts.push(moveCount);
-            if (endMoveCounts.length > updateMetadataInterval) {
-                endMoveCounts.shift();
-            }
-            const reward = gameManager.score - calculateAvg(absoluteScores)
-            agent.learn(reward);
-            plotGameMetadata();
-            moveCount = 0;
-            direction0Count = 0;
-            direction1Count = 0;
-            direction2Count = 0;
-            direction3Count = 0;
             gameManager.restart();
         }
 
@@ -234,73 +97,10 @@ let db = new PouchDB('http://localhost:5984/network');
 let simulation;
 
 $('#startSim').on('click', () => {
-
-    scoreChart = new CanvasJS.Chart("scoreChart", {
-        title: {
-            text: "endscores"
-        },
-        data: [{
-            type: "line",
-            showInLegend: true,
-            name: "absolute",
-            dataPoints: absoluteScores
-        },
-            {
-                type: "line",
-                showInLegend: true,
-                name: "average (last 100 games)",
-                dataPoints: averageScores
-            }],
-    });
-    scoreChart.render();
-
-    rewardChart = new CanvasJS.Chart("rewardChart", {
-        title: {
-            text: "reward"
-        },
-        data: [{
-            type: "line",
-            showInLegend: true,
-            name: "absolute",
-            dataPoints: absoluteRewards
-        }],
-    });
-    rewardChart.render();
-
-    directionChart = new CanvasJS.Chart("directionChart", {
-        title: {
-            text: "directions taken per game"
-        },
-        data: [{
-            type: "line",
-            showInLegend: true,
-            name: "0",
-            dataPoints: direction0History
-        },{
-            type: "line",
-            showInLegend: true,
-            name: "1",
-            dataPoints: direction1History
-        },{
-            type: "line",
-            showInLegend: true,
-            name: "2",
-            dataPoints: direction2History
-        },{
-            type: "line",
-            showInLegend: true,
-            name: "3",
-            dataPoints: direction3History
-        }],
-
-    });
-    directionChart.render();
-
     if (gameManager.over === false) {
         simulation = initiateLearning();
     } else {
         gameCount++;
-        lastRatings.push(gameManager.score);
         gameManager.restart();
     }
 });
