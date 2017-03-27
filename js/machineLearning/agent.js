3303caf7c2feb16000ae66c4bfac261ad4c7966a
Chimney42 <draggili@gmail.com>
2017-03-27 19:44:41 +0200

    JSUnconf 2017

--- js/machineLearning/agent.js
+++ js/machineLearning/agent.js
@@ -35,13 +35,23 @@ calculateHighestTile = () => {
 normalize = (val, max, min) => (val - min) / (max - min);
 
 let rewards = [];
-const calculateAverageReward = () => {
+const calculateAverageRewards = () => {
     return rewards.reduce((acc, val) => acc + val, 0) / rewards.length;
 };
 
 const scores = [];
 const calculateAverageScores = () => {
     return scores.reduce((acc, val) => acc + val, 0) / scores.length;
+};
+
+const moves = [];
+const calculateAverageMoves = () => {
+    return moves.reduce((acc, val) => acc + val, 0) / moves.length;
+};
+
+const ratings = [];
+const calculateAverageRatings = () => {
+    return ratings.reduce((acc, val) => acc + val, 0) / ratings.length;
 };
 
 calculateReward = (action) => {
@@ -73,15 +83,15 @@ let agent;
 const spec = {
     update: 'qlearn', // qlearn | sarsa
     gamma: 0.5, // discount factor, [0, 1)
-    epsilon: 0.15, // initial epsilon for epsilon-greedy policy, [0, 1)
+    epsilon: 0.2, // initial epsilon for epsilon-greedy policy, [0, 1)
     alpha: 0.02, // value function learning rte
     experience_add_every: 5, // number of time steps before we add another experience to replay memory
     experience_size: 100000, // size of experience replay memory
     learning_steps_per_iteration: 100,
-    //spec.tderror_clamp = 1.0; // for robustness
+    //tderror_clamp : 1.0; // for robustness
     num_hidden_units: 256 // number of neurons in hidden layer
 };
-
+let minEpsilon = 0.05;
 let gameCount = 1;
 let moveCount = 1;
 
@@ -93,26 +103,52 @@ const move = (next) => {
 
         moveCount++;
 
-        logActionMeasurement(gameCount, action)
-            .then(() => next());
+        setTimeout(next, 0)
     } else {
         const moveNo = moveCount;
         moveCount = 1;
-        const gameNo = gameCount;
+
+        moves.push(moveNo);
+        if (moves.length > 100) {
+            moves.shift();
+        }
+        const averageMoves = calculateAverageMoves();
+
         const score = gameManager.score;
         scores.push(score);
         if (scores.length > 100) {
             scores.shift();
         }
+        const averageScores = calculateAverageScores();
 
-        const reward = calculateRating();
+        const rating = calculateRating();
+        ratings.push(rating);
+        if(ratings.length > 100) {
+            ratings.shift();
+        }
+        const averageRatings = calculateAverageRatings();
+
+        const reward = (rating / averageRatings) * (score / averageScores);
+        rewards.push(reward);
+        if(rewards.length > 100) {
+            rewards.shift();
+        }
+        const averageRewards = calculateAverageRewards();
+
         agent.learn(reward); // the agent improves its Q,policy,model, etc.
 
         gameManager.restart();
+        const gameNo = gameCount;
+        if (gameCount % 1000 === 0) {
+            console.log(agent.epsilon);
+            agent.epsilon = agent.epsilon - ((agent.epsilon - minEpsilon) / 2);
+        }
         gameCount++;
-
-        logGameMeasurement(gameNo, score, moveNo, reward)
-            .then(() => next());
+        setTimeout(next, 0);
+        console.log('score', score);
+        console.log('avgScore', averageScores);
+        //logGameMeasurement(gameNo, reward, averageRewards, rating, averageRatings, score, averageScores, moveNo, averageMoves)
+            //.then(() => next());
     }
 };
 
@@ -130,11 +166,12 @@ let simulation;
 
 const saveAgentToCouch = () => {
     const document = agent.toJSON();
-    document._id = 'ratingGameEnd';
+    document._id = 'jsunconf';
     db.get(document._id).then(function (doc) {
         document._rev = doc._rev;
         return db.put(document);
     }).catch(function (err) {
+        console.log(err);
         db.put(document);
     });
 }
@@ -158,7 +195,7 @@ $('#saveAgent').on('click', () => {
 });
 
 $('#loadAgent').on('click', () => {
-    const document = db.get('ratingGameEnd');
+    const document = db.get('decayEpsilonRatSco');
     agent = agent || new RL.DQNAgent(env, spec);
     agent = agent.fromJSON(document);
 });
--- js/machineLearning/influx.js
+++ js/machineLearning/influx.js
@@ -1,4 +1,4 @@
-const dbName = 'ratingGameEnd';
+const dbName = 'decayEpsilonRatSco';
 const urlBase = 'http://localhost:8086/';
 
 const writeToInflux = (dataPoint) => {
@@ -9,16 +9,16 @@ const writeToInflux = (dataPoint) => {
     });
 };
 
-const logGameMeasurement = (gameCount, score, moveCount, reward) => {
+const logGameMeasurement = (gameCount, reward, averageRewards, rating, averageRatings, score, averageScores, moveCount, averageMoves) => {
     return new Promise((resolve, reject) => {
-        const dataPoint = 'game,gameCount='+gameCount+' score='+score+',reward='+reward+',moveCount='+moveCount;
+        const dataPoint = 'game,gameCount='+gameCount+' reward='+reward+',averageReward='+averageRewards+',rating='+rating+',averageRating='+averageRatings+',score='+score+',averageScore='+averageScores+',moveCount='+moveCount+',averageMove='+averageMoves;
         writeToInflux(dataPoint)
             .then(resolve);
     });
 
 };
 
-const logActionMeasurement = (gameCount, action     ) => {
+const logActionMeasurement = (gameCount, action) => {
     return new Promise((resolve, reject) => {
         const dataPoint = 'action,gameCount='+gameCount+',direction='+action+' value=1';
         writeToInflux(dataPoint)
